8784c19fd998e56468e9ba5bcf6bdf86268b4070
Panos Orovas <panagiwtis.orovas@gmail.com>
2025-07-30 21:46:26 +0300

    fixes

--- src/views/Lobby/components/ParticipantsModal.tsx
+++ src/views/Lobby/components/ParticipantsModal.tsx
@@ -16,7 +16,13 @@ interface Props {
   participantsInCall: Participant[];
 }
 
-const ParticipantItem = ({ participant }: { participant: Participant }) => {
+const ParticipantItem = ({
+  participant,
+  isLobby,
+}: {
+  participant: Participant;
+  isLobby?: boolean;
+}) => {
   return (
     <div key={participant.id}>
       <div className='flex items-center gap-4 p-4 rounded-8 hover:bg-gray-100 duration-150'>
@@ -28,6 +34,9 @@ const ParticipantItem = ({ participant }: { participant: Participant }) => {
             <span>{participant.username}</span>
             {participant.isHost && (
               <Crown size={12} className='text-yellow-600' />
+            )}
+            {isLobby && (
+              <span className='text-xs text-gray-500'>Waiting in lobby</span>
             )}
           </div>
         </div>
@@ -60,22 +69,26 @@ const ParticipantsModal = ({
         </DialogHeader>
         <Separator />
         <div className='flex gap-24 mt-8'>
-          <div className='flex flex-col gap-4 w-full'>
-            <div className='text-xs font-medium text-gray-600'>
-              IN CALL ({participantsInCall.length})
+          {participantsInCall.length > 0 && (
+            <div className='flex flex-col gap-4 w-full'>
+              <div className='text-xs font-medium text-gray-600'>
+                IN CALL ({participantsInCall.length})
+              </div>
+              <div className='flex flex-col gap-4 text-sm'>
+                {participantsInCall.map((participant) => (
+                  <ParticipantItem
+                    key={participant.id}
+                    participant={participant}
+                  />
+                ))}
+              </div>
             </div>
-            <div className='flex flex-col gap-4 text-sm'>
-              {participantsInCall.map((participant) => (
-                <ParticipantItem
-                  key={participant.id}
-                  participant={participant}
-                />
-              ))}
-            </div>
-          </div>
+          )}
+          {participantsInCall.length > 0 && participantsInLobby.length > 0 && (
+            <Separator orientation='vertical' />
+          )}
           {totalUsersLobby > 0 && (
             <>
-              <Separator orientation='vertical' />
               <div className='flex flex-col gap-4 text-left w-full'>
                 <div className='text-xs text-left font-medium text-gray-600'>
                   IN LOBBY ({totalUsersLobby})
@@ -85,6 +98,7 @@ const ParticipantsModal = ({
                     <ParticipantItem
                       key={participant.id}
                       participant={participant}
+                      isLobby
                     />
                   ))}
                 </div>
