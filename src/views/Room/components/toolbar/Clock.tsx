5abc8604ff04274c3bef711bc4b16508daa453b0
Panos Orovas <panagiwtis.orovas@gmail.com>
2025-08-22 02:28:50 +0300

    add clock/connection status & timer

---
+++ src/views/Room/components/toolbar/Clock.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+import { format } from 'date-fns';
+
+const Clock = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div className='w-72 text-lg text-white font-medium'>
+      {format(currentTime, 'h:mm aa')}
+    </div>
+  );
+};
+
+export default Clock;
