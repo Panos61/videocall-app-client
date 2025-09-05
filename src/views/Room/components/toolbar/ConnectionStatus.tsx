290324904aa1891ed1716d1e71a4a423083ed969
Panos Orovas <panagiwtis.orovas@gmail.com>
2025-09-06 00:21:53 +0300

    minor

--- src/views/Room/components/toolbar/ConnectionStatus.tsx
+++ src/views/Room/components/toolbar/ConnectionStatus.tsx
@@ -4,7 +4,7 @@ import { SignalIcon } from 'lucide-react';
 const ConnectionStatus = () => {
   return (
     <div className='flex items-center gap-8'>
-      <SignalIcon size={20} className='text-green-500 stroke-3' />
+      <SignalIcon size={18} className='text-green-500 stroke-3' />
       <p className='text-xs text-white'>Connection</p>
     </div>
   );
