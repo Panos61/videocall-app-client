1d345aa38552d58c6c9d25b8ce8859ad635d6d56
Panos Orovas <panagiwtis.orovas@gmail.com>
2025-09-29 21:30:56 +0300

    update sidebar chat design

--- src/views/Room/Sidebar.tsx
+++ src/views/Room/Sidebar.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from 'react';
 import classNames from 'classnames';
 import { usePreferencesCtx } from '@/context';
-import { X } from 'lucide-react';
+import { LockIcon, X } from 'lucide-react';
 
 interface Props {
   title: string;
@@ -20,20 +20,30 @@ const Sidebar = ({ title, children, open, onClose }: Props) => {
       'translate-x-0 opacity-100 visible': open,
       'translate-x-full opacity-0 invisible': !open,
       'w-[340px]': !isChatExpanded,
-      'w-[800px]': isChatExpanded ,
+      'w-[800px]': isChatExpanded,
     }
   );
 
   return (
-    <div className={cls}>
-      <div className='flex flex-col h-full mx-8'>
-        <div className='flex items-center justify-between p-16'>
-          <p className='text-white'>{title}</p>
-          <X size='20px' onClick={onClose} className='text-white' />
+    <>
+      <div className={cls}>
+        <div className='flex flex-col h-full mx-8'>
+          <div className='flex items-center justify-between p-16'>
+            <p className='text-white'>{title}</p>
+            <X size='20px' onClick={onClose} className='text-white' />
+          </div>
+          {children}
+          {title === 'Conversation' && (
+            <div className='flex items-center'>
+              <span className='m-8 text-xs text-slate-300'>
+                Conversation is encrypted
+              </span>
+              <LockIcon size={16} className='text-purple-700' />
+            </div>
+          )}
         </div>
-        {children}
       </div>
-    </div>
+    </>
   );
 };
 
