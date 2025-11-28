4542447932c913b60dbea5c5751bf2da9e1212f8
Panos Orovas <panagiwtis.orovas@gmail.com>
2025-11-28 16:26:48 +0200

    rename folder name to kebab-case format

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,7 +1,7 @@
 import Home from './Home';
 import Authorization from './Authorization';
 import Lobby from './Lobby';
-import Room from './Room';
+import Room from './room';
 import PostCall from './post-call';
 import { Invalid, NotFound } from './Invalid';
 
